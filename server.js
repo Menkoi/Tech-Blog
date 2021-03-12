4d6e69433968031e6df33ce4af7fbbb9807a3f89
celaeon <celaeon@gmail.com>
2021-03-12 11:57:00 -0500

    added api routes

--- server.js
+++ server.js
@@ -1,39 +1,40 @@
 const path = require('path');
 const express = require('express');
-const session = require('express-session');
-const exphbs = require('express-handlebars');
-
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars'); 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const session = require('express-session');
 
-const sequelize = require("./config/connection");
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
   secret: 'Super secret secret',
-  cookie: {},
+  cookie: {maxAge: 6000},
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
-    db: sequelize
+    db: sequelize,
   })
 };
 
 app.use(session(sess));
-
 const helpers = require('./utils/helpers');
-
 const hbs = exphbs.create({ helpers });
-
+//template engine
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+const PORT = process.env.PORT || 3001;
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(require('./controllers/'));
+//routes
+app.use(routes);
 
+//connection to db
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
