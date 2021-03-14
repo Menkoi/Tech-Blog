7e51b50d061943b403ac7b605232461127bf65d5
celaeon <celaeon@gmail.com>
2021-03-13 22:27:46 -0500

    changed to false

--- server.js
+++ server.js
@@ -28,13 +28,13 @@ app.set('view engine', 'handlebars');
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 //routes
 app.use(routes);
 
 //connection to db
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
